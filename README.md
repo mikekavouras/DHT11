02a93848b458d9b4e62445a605fb42d1a6cd9597
Mike Kavouras <mikekavouras@users.noreply.github.com>
2017-08-27 16:16:29 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 A simple Arduino project for printing temp/humidity.
 
+![image](http://www.circuitbasics.com/wp-content/uploads/2015/10/Arduino-DHT11-Tutorial-4-Pin-DHT11-Wiring-Diagram.png)
+
 
 **Deps:**  
 https://github.com/adafruit/Adafruit_Sensor  
--- main.ino
+++ main.ino
@@ -2,17 +2,12 @@
 #include "DHT.h"
 #include "DHT_U.h"
 
-#include <DHT.h>
-#include <DHT_U.h>
-
 #define DHT_11_PIN A0
 #define DHTTYPE DHT11
 
 DHT dht(DHT_11_PIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-
   Serial.begin(9600);
   Serial.println("DHT11 test");
 
@@ -20,15 +15,18 @@ void setup() {
 }
 
 void loop() {
-
   float humidity = dht.readHumidity();
   float temp = dht.readTemperature(true);
 
-  Serial.print("Temperature = ");
-  Serial.println(temp);
+  if (isnan(humidity) || isnan(temp)) {
+    Serial.print("Invalid reading...");
+  } else {
+    Serial.print("Temperature = ");
+    Serial.println(temp);
 
-  Serial.print("Humidity = ");
-  Serial.println(humidity);
+    Serial.print("Humidity = ");
+    Serial.println(humidity);
+  }
 
   delay(1000);
 }
